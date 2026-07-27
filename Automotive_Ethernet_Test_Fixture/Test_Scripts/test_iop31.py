d7468d2f4022811b6cb6563e908255fa94f39c76
rachakravorty <rchakravorty@iol.unh.edu>
2026-07-27 10:52:59 -0400

    Add IOP_31 test case to test_iop31.py
    Implement IOP_31 test with PHY information checks.

--- Automotive_Ethernet_Test_Fixture/Test_Scripts/test_iop31.py
+++ Automotive_Ethernet_Test_Fixture/Test_Scripts/test_iop31.py
@@ -1,1 +1,33 @@
+from base_test import BaseTest
 
+class TestIOP31(BaseTest):
+
+    def run(self):
+
+        print("Running IOP_31")
+
+        # Configure fixture
+        self.arduino.normal()
+
+        # Allow link to stabilize
+        # (time delay to be determined from your hardware)
+        # time.sleep(...)
+
+        # Read PHY information
+        link = self.mdio.read_link_status()
+        cable = self.mdio.read_cable_status()
+        errors = self.mdio.read_error_flags()
+
+        passed = (
+            link and
+            cable == "GOOD" and
+            errors == 0
+        )
+
+        return {
+            "test": "IOP_31",
+            "passed": passed,
+            "link": link,
+            "cable": cable,
+            "errors": errors
+        }

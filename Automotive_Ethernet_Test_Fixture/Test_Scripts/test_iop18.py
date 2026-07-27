396810686eb0107c4cb2f93b453eb306dfe5d5c4
rachakravorty <rchakravorty@iol.unh.edu>
2026-07-27 10:53:45 -0400

    Add IOP_18 test case to test_iop18.py

--- Automotive_Ethernet_Test_Fixture/Test_Scripts/test_iop18.py
+++ Automotive_Ethernet_Test_Fixture/Test_Scripts/test_iop18.py
@@ -1,1 +1,31 @@
 
+from base_test import BaseTest
+
+class TestIOP18(BaseTest):
+
+    def run(self):
+
+        print("Running IOP_18")
+
+        self.arduino.polarity()
+
+        # Wait for DUT to react
+        # time.sleep(...)
+
+        link = self.mdio.read_link_status()
+
+        polarity = self.mdio.read_polarity_status()
+
+        passed = (
+            (not link) and
+            polarity
+        )
+
+        self.arduino.normal()
+
+        return {
+            "test": "IOP_18",
+            "passed": passed,
+            "link": link,
+            "polarity_detected": polarity
+        }
